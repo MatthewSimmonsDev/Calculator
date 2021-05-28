41333e2c41de2c703fa905818a7e9b6ac1d123d7
Matthew Simmons <matthewsimmonsdev@gmail.com>
2021-05-28 11:07:42 -0400

    Refactored coded, added keyboard functionality and fixed bugs.

--- app.js
+++ app.js
@@ -7,9 +7,10 @@ const equalsBtn = document.querySelector('.equals');
 const clrBtn = document.querySelector('#clear-button');
 
 let storedValue = '';
-let secondValue = '';
 let storedOperator = '';
 let parseOne, parseTwo;
+
+
 
 function add(num1, num2){
   newText.textContent = num1 + num2;
@@ -27,13 +28,18 @@ function multiply(num1, num2){
 }
 
 function divide(num1, num2){
+  if (num2 === 0){
+    alert("OMG what have you done!");
+    location.reload();
+  }
+  else
   newText.textContent = num1 / num2;
   oldText.textContent = num1 + " / " + num2;
 }
 
 function toNum(){
-  parseOne = parseInt(oldText.textContent);
-  parseTwo = parseInt(storedValue);
+  parseOne = parseFloat(oldText.textContent);
+  parseTwo = parseFloat(storedValue);
 }
 
 function calculate(){
@@ -57,33 +63,65 @@ function calculate(){
   }
 
   storedValue = newText.textContent;
+  storedOperator = '';
+}
+
+function keyPress(e){
+  console.log(e.key);
+  if (e.key >= 0  && e.key <= 9) placeInt(e.key);
+  if (e.key === ".") placeInt(e.key);
+  if (e.key === "/" || e.key === "*" || e.key === "-" || e.key === "+") placeOperator(e.key);
+  if (e.key === "Escape" || e.key === "Backspace") clear();
+  if (e.key === "=" || e.key === "Enter") calculate();
+}
+
+function placeInt(num){
+  newText.textContent = '';
+  if (storedValue.includes(".") && num.textContent === "." || storedValue.includes(".") && num === "."){
+    newText.textContent = '';
+  }  
+  else if(typeof num === typeof "number") storedValue += num;
+  else storedValue += num.textContent;
+  newText.textContent += storedValue;
+}
+
+function placeOperator(operator){
+  if(storedValue){
+    oldText.textContent = '';
+    if(operator === "/" || operator === "*" || operator === "-" || operator === "+") storedOperator = operator;
+    else storedOperator = operator.textContent;
+    newText.textContent += ' ' + storedOperator;
+    oldText.textContent += storedValue;
+    storedValue = '';
+  }
+}
+
+function clear(){
+  storedValue = '';
+  newText.textContent = '0';
+  oldText.textContent = '0';
 }
 
 integers.forEach((item) => {
   item.addEventListener('click', () => {
-    newText.textContent = '';
-    storedValue += item.textContent;
-    newText.textContent += storedValue;
+    placeInt(item);
   })
 })
 
 operators.forEach((item) => {
   item.addEventListener('click', () => {
-    oldText.textContent = '';
-    storedOperator = item.textContent;
-    newText.textContent += ' ' + storedOperator;
-    oldText.textContent += storedValue;
-    storedValue = '';
+    calculate();
+    placeOperator(item);
   })
 })
 
 clrBtn.addEventListener('click', () => {
-  storedValue = '';
-  newText.textContent = '0';
-  oldText.textContent = '0';
+  clear();
 })
 
 equalsBtn.addEventListener('click', calculate);
+
+window.addEventListener('keydown', keyPress);
 
 
 
